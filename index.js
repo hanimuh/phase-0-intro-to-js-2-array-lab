c8a50a892523d5a96067c8d4e7cd6aa86a933114
Hani <md.hani@hotmail.com>
2022-04-20 04:42:25 +0300

    commit tree 820ad30dc8e5243cae05fc7c68874846da422ba6

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // Write your solution here!
+const cats=[]
+function beforeEach (){
+    cats.length=0;
+    cats.push ("Milo", "Otis", "Garfield");
+}
+beforeEach ('Milo', 'Otis', 'Garfield')
+function destructivelyAppendCat(name){
+    cats.push (name);
+}
+destructivelyAppendCat("Ralph")
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+destructivelyPrependCat("Bob")
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+destructivelyRemoveLastCat()
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+destructivelyRemoveFirstCat()
+
+
+function appendCat (name){
+    let newArr=[];
+    for (let item of cats){
+        newArr.push (item);
+    }
+    newArr.push (name)
+    return newArr;
+}
+appendCat ("Broom")
+
+function prependCat(name){
+    let newArr=[];
+    newArr.push (name);
+    for (let item of cats){
+        newArr.push (item);
+    }
+    return newArr;
+}
+prependCat("Arnold")
+
+function removeLastCat(){
+    let newArr=[];
+    for (let item of cats){
+        newArr.push (item);
+    }
+    newArr.pop();
+    return newArr;
+}
+removeLastCat()
+
+function removeFirstCat(){
+    let newArr=[];
+    for (let item of cats){
+        newArr.push (item);
+    }
+    newArr.shift();
+    return newArr;
+}
+removeFirstCat()
+
+
+console.log (removeFirstCat())
